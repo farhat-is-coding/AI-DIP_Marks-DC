12966c188ced6e78f606a31d765f46060aeb2460
Iz X <200779@students.au.edu.pk>
2023-05-13 17:59:42 +0500

    model integrated with gui

---
+++ final/main.py
@@ -0,0 +1,90 @@
+# this is the startting point of the application
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image
+from PIL import ImageTk
+from PIL.ImageTk import PhotoImage
+import os
+from model_work import run_model
+
+window = tk.Tk()
+
+window.geometry("1000x600")
+
+# Define function to handle file upload
+
+
+def upload_image():
+    # Only allow JPG and PNG files
+    filetypes = [("JPEG Image", "*.jpg"), ("PNG Image", "*.png")]
+    filename = filedialog.askopenfilename(filetypes=filetypes)
+    if filename:
+        # Display selected image in the image container
+        image = ImageTk.PhotoImage(Image.open(filename).resize((400, 400)))
+        # canvas.itemconfig(image_item, image=image
+        image_item.config(image=image)
+        canvas.image_item = image
+        # image taken - now send to model
+        run_model(filename)
+        print(filename)
+
+
+
+# Create a Canvas widget
+canvas = tk.Canvas(window, bg="#FFFFFF", width=1000, height=600,
+                   bd=0, highlightthickness=0, relief="ridge")
+canvas.place(x=0, y=0)
+
+# getting the path to the current working directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# background image
+bg_image_path = os.path.join(script_dir, 'bg_img.jpg')
+background_image = ImageTk.PhotoImage(Image.open(bg_image_path))
+canvas.create_image(0, 0, anchor=tk.NW, image=background_image)
+
+canvas.create_text(200, 10, anchor="nw", text="Marks Detector & Calculator",
+                   fill="#FFFFFF", font=("Times New Roman", 35))
+
+# Create button to upload image
+upload_button = tk.Button(window, text="Upload Image", command=upload_image, height=2,
+                          borderwidth=5, highlightthickness=10, font=("Times New Roman",12, "bold"), bg="#6CB4EE", fg="white")
+upload_button.place(x=250, y=80, width=150, height=40)
+
+# Create button to add to Excel file
+add_to_excel_button = tk.Button(window, text="Add to Excel File", height=2, borderwidth=5,
+                                highlightthickness=10, font=("Times New Roman", 12, "bold"), bg="#6CB4EE", fg="white")
+add_to_excel_button.place(x=680, y=80, width=150, height=40)
+
+# gojo_image_path = os.path.join(script_dir, 'gojo.jpg')
+# default_image = ImageTk.PhotoImage(Image.open(gojo_image_path).resize((550, 500)))
+# image_item = canvas.create_image(100, 100, anchor="nw", image=default_image)
+
+# Create a frame as a child of the canvas
+frame0 = tk.Frame(canvas, bg='white', bd=5)
+# frame.place(relx=0.5, rely=0.5, anchor='center')
+# place the frame at 100,100
+frame0.place(x=100, y=120, width=400, height=400)
+
+# Load the image and create an ImageTk object
+gojo_image_path = os.path.join(script_dir, 'gojo.jpg')
+default_image = ImageTk.PhotoImage(
+    Image.open(gojo_image_path).resize((400, 400)))
+
+# Create an image item as a child of the frame
+image_item = tk.Label(frame0, image=default_image)
+image_item.pack()
+
+# Create a window inside the canvas to hold the frame
+canvas.create_window(100, 120, anchor='nw', window=frame0)
+
+
+# frame for the text area
+frame = tk.Frame(canvas, bg='white', bd=5)
+frame.place(x=550, y=120, width=400, height=400)
+# Create text area for information
+info_text = tk.Text(frame, height=20, width=50)
+# info_text.place(x=500, y=100, width=400, height=400)
+info_text.pack()
+
+window.mainloop()
--- final/model_work.py
+++ final/model_work.py
@@ -1,94 +1,96 @@
-# cloning the yolov5 repo
-# only needs to be run once
-# import subprocess
+def run_model(img_input):
+    # cloning the yolov5 repo
+    # only needs to be run once
+    # import subprocess
 
-# # Run the "git clone" command
-# subprocess.run(['git', 'clone', 'https://github.com/ultralytics/yolov5.git'])
+    # # Run the "git clone" command
+    # subprocess.run(['git', 'clone', 'https://github.com/ultralytics/yolov5.git'])
 
-# making the repo our working directory (in cmd)
-# change as necessary!!!
-import os
-os.chdir('E:/University/Semester 6 - Spring 2023/Digital Image Processing/LabWork/dip_project/AI-DIP_Marks-DC/final/yolov5')
+    # making the repo our working directory (in cmd)
+    # change as necessary!!!
 
-
-# also needs to be run only once to satisfy requirements
-# subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
-
-print("")
-
-# imports
-import torch
-import torchvision
-from PIL import Image
-import cv2
-import numpy as np
-
-# Load the YOLOv5 model
-print(os.getcwd()) # gets the current working directory
-
-# getting the path - to allow the app to run on any machine
-parent_dir = os.path.dirname(os.getcwd()) 
-print(f"-------------\n{parent_dir}\n---------------")
-model_name = parent_dir + "/best.pt" 
-
-# loading the model into a pytorch object
-model = torch.hub.load(os.getcwd(), 'custom', source='local', path = model_name, force_reload = True)
-print("")
-
-# Prepare the input image
-img_name = "18.jpg" # will use this below
-
-img_path = f'{parent_dir}/{img_name}'
-img = Image.open(img_path) 
-
-# running the model on the image
-results = model(img) # returns batch of images
-
-crops = results.crop(save=True)  # cropped detections dictionary
+    import os
+    os.chdir('E:/University/Semester 6 - Spring 2023/Digital Image Processing/LabWork/dip_project/AI-DIP_Marks-DC/final/yolov5')
 
 
-# listing where the processed image is stored
-folder_path = 'runs/detect'
-subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]   
-print(subfolders)
-latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
-print(latest_subfolder)
-image_path = folder_path+'/'+latest_subfolder 
-print(image_path)
+    # also needs to be run only once to satisfy requirements
+    # subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+
+    print("")
+
+    # imports
+    import torch
+    import torchvision
+    from PIL import Image
+    import cv2
+    import numpy as np
+
+    # Load the YOLOv5 model
+    print(os.getcwd()) # gets the current working directory
+
+    # getting the path - to allow the app to run on any machine
+    parent_dir = os.path.dirname(os.getcwd()) 
+    print(f"-------------\n{parent_dir}\n---------------")
+    model_name = parent_dir + "/best.pt" 
+
+    # loading the model into a pytorch object
+    model = torch.hub.load(os.getcwd(), 'custom', source='local', path = model_name, force_reload = True)
+    print("")
+
+    # Prepare the input image
+    img_name = img_input # will use this below
+
+    # img_path = f'{parent_dir}/{img_name}'
+    img = Image.open(img_name) 
+
+    # running the model on the image
+    results = model(img) # returns batch of images
+
+    crops = results.crop(save=True)  # cropped detections dictionary
+
+
+    # listing where the processed image is stored
+    folder_path = 'runs/detect'
+    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]   
+    print(subfolders)
+    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
+    print(latest_subfolder)
+    image_path = folder_path+'/'+latest_subfolder 
+    print(image_path)
 
 
 
-# listing where the sropped images are stored
-crop_images_path = image_path+'/crops' 
-print(crop_images_path)
+    # listing where the sropped images are stored
+    crop_images_path = image_path+'/crops' 
+    print(crop_images_path)
 
-crop_subfolders = [f for f in os.listdir(crop_images_path) if os.path.isdir(os.path.join(crop_images_path, f))]   
+    crop_subfolders = [f for f in os.listdir(crop_images_path) if os.path.isdir(os.path.join(crop_images_path, f))]   
 
-print(crop_subfolders)
-# marks_box
-marks_box_path = f"{crop_images_path}/{crop_subfolders[0]}/{img_name}"
-roll_no_box_path = f"{crop_images_path}/{crop_subfolders[1]}/{img_name}"
-subject_name_path = f"{crop_images_path}/{crop_subfolders[2]}/{img_name}"
+    print(crop_subfolders)
+    # marks_box
+    marks_box_path = f"{crop_images_path}/{crop_subfolders[0]}/{img_name}"
+    roll_no_box_path = f"{crop_images_path}/{crop_subfolders[1]}/{img_name}"
+    subject_name_path = f"{crop_images_path}/{crop_subfolders[2]}/{img_name}"
 
-print(marks_box_path)
-print(roll_no_box_path)
-print(subject_name_path)
+    print(marks_box_path)
+    print(roll_no_box_path)
+    print(subject_name_path)
 
 
-# the above paths have them images
+    # the above paths have them images
 
-roll_no_img = cv2.imread(roll_no_box_path)
-subject_name_img = cv2.imread(subject_name_path)
-marks_box_img = cv2.imread(marks_box_path)
+    roll_no_img = cv2.imread(roll_no_box_path)
+    subject_name_img = cv2.imread(subject_name_path)
+    marks_box_img = cv2.imread(marks_box_path)
 
-cv2.imshow('roll no img', roll_no_img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+    # cv2.imshow('roll no img', roll_no_img)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
 
-cv2.imshow('subject name', subject_name_img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+    # cv2.imshow('subject name', subject_name_img)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
 
-cv2.imshow('marks box', marks_box_img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+    # cv2.imshow('marks box', marks_box_img)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
